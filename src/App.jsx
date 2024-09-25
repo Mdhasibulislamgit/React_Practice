65f9ce03087138f25677fd42857af7b97b9625c7
mdhasibulislamgit <mhislamemail@gmail.com>
2024-09-25 20:33:29 +0600

    rendering array with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-
-import Product from './component/Product';
+import React from 'react'
 
 const App = () => {
-  const product1 = {
-    name: "John Doe",
-    age: 28,
-    email: "john.doe@example.com",
-  };
 
+  const list = ["apple", "banana", "cherry"];
   return (
     <div>
-      <h1>User Information</h1>
-      <p>Name: {product1.name}</p>
-      <p>Age: {product1.age}</p>
-      <p>Email: {product1.email}</p>
-      
-      <Product product={{ name: "Laptop", price: 50000 }} />
+      {list.map((item, index) => <p key={index}>{item} {index}</p>)}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
